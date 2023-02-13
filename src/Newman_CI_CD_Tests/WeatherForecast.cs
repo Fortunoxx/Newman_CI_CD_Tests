154b637bf72ac8b7b994f7ad586154c452309f68
Rob Gates <robert_goetz@hotmail.com>
2023-02-13 20:27:23 +0100

    initial

---
+++ src/Newman_CI_CD_Tests/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Newman_CI_CD_Tests;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
